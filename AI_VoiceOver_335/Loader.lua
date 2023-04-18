d96a0c2c9d582181605b3f61425745b61314da37
xvwyh <43143822+xvwyh@users.noreply.github.com>
2023-04-18 04:15:43 +0300

    Discarded solution to having a single release that's compatible with 1.12, 3.3.5, 1.14, 3.4 and mainline

---
+++ AI_VoiceOver_335/Loader.lua
@@ -0,0 +1,68 @@
+local ADDON_CORE   = "AI_VoiceOver"
+local ADDON_LOADER = "AI_VoiceOver_335"
+local ADDON_LIBS   = "AI_VoiceOver_335_Libs"
+
+-- Load this file in 3.3.5 only
+local _, _, _, version = GetBuildInfo()
+if version ~= 30300 then
+    DisableAddOn(ADDON_LOADER)
+    return
+end
+
+-- The rest of this file is the same for all loaders
+StaticPopupDialogs["VOICEOVER_LOADER_ERROR"] =
+{
+    text = format("VoiceOver|n|n%s %%s", (GetAddOnInfo(ADDON_LOADER))),
+    button1 = OKAY,
+    timeout = 0,
+    whileDead = 1,
+}
+
+local function Load(addon)
+    if IsAddOnLoaded(addon) then
+        return
+    end
+
+    local _, _, _, enabled, loadable, reason = GetAddOnInfo(addon)
+
+    -- We forcibly enable disabled and out-of-date addons, so we can ignore these reasons
+    if reason == "DISABLED" or reason == "INTERFACE_VERSION" then
+        reason = nil
+        loadable = true
+    end
+
+    if reason == "MISSING" then
+        StaticPopup_Show("VOICEOVER_LOADER_ERROR", format([[could not find the addon "%s".|nThis addon is required to run VoiceOver on this version of WoW.|nPlease verify that you installed VoiceOver correctly.]], addon))
+        return
+    end
+
+    if reason then
+        StaticPopup_Show("VOICEOVER_LOADER_ERROR", format([[failed to load the addon "%s". The reason given was "%s".|nThis addon is required to run VoiceOver on this version of WoW.|nPlease verify that you installed VoiceOver correctly.]], addon, getglobal("ADDON_" .. reason)))
+        return
+    end
+
+    if not loadable then
+        StaticPopup_Show("VOICEOVER_LOADER_ERROR", format([[failed to load the addon "%s" for an unknown reason.|nThis addon is required to run VoiceOver on this version of WoW.|nPlease verify that you installed VoiceOver correctly.]], addon))
+        return
+    end
+
+    if not IsAddOnLoadOnDemand(addon) then
+        StaticPopup_Show("VOICEOVER_LOADER_ERROR", format([[failed to load the addon "%s" because it's not marked as LoadOnDemand.|nYou may have forgotten to install or update one of the addons included in the package.|nPlease verify that you installed VoiceOver correctly.]], addon))
+        return
+    end
+
+    if not enabled then
+        EnableAddOn(addon)
+    end
+
+    local oldLoadOutOfDateAddons = GetCVar("checkAddonVersion")
+    SetCVar("checkAddonVersion", 0)
+    local loaded = LoadAddOn(addon)
+    SetCVar("checkAddonVersion", oldLoadOutOfDateAddons)
+
+    return loaded
+end
+
+if Load(ADDON_LIBS) then
+    Load(ADDON_CORE)
+end
